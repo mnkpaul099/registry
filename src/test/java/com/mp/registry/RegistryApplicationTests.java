32d39c567008475deaf26a22d28126567a7a86a1
mnkpaul099 <mnkpaul099@gmail.com>
2024-12-12 00:21:48 +0530

    commit to master

---
+++ src/test/java/com/mp/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
